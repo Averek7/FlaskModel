391de36b1caae77306a49b97c9a8ee83de3ddfdb
Averek <singhavanish2003@gmail.com>
2023-02-28 11:44:45 +0530

    initiates

---
+++ model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+
+data = pd.read_csv('flaskmodel/Salary_Data.csv')
+
+from sklearn.linear_model import LinearRegression
+
+print(data)
