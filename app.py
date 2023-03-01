1f9eae1ff30818ec641b61f460e261996436517b
Averek <singhavanish2003@gmail.com>
2023-03-01 11:50:22 +0530

    local-test-success

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ import numpy as np
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-model = pickle.load(open('flaskmodel/model.pkl', 'rb'))
+model = pickle.load(open('./model.pkl', 'rb'))
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
-@app.route('/predict', method=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
     ini_feature = [int(x) for x in request.form.values()]
     final_features = [np.array(ini_feature)]
     pred = model.predict(final_features)
 
     output = round(pred[0], 2)
-    return render_template('index.html', prediction_text="Salary for YOE should be ${}".format(output))
+
+    # return render_template('index.html', prediction_text=)
+    return "Salary for YOE should be ${}".format(output)
 
 
 if __name__ == "__main__":
